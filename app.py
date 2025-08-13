af6db6e95ee8f60fbf30853c61225992a67cc320
Gustavo Pires <gsdpires@gmail.com>
2025-08-12 22:02:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import streamlit as st
-import pandas as pd
-import plotly.express as px
+import streamlit as st # type: ignore
+import pandas as pd # type: ignore
+import plotly.express as px # type: ignore
 
 # =========================
 # CONFIGURAÇÃO DA PÁGINA
